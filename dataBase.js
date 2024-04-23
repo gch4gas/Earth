43b833989fb6747cea2c7b845925df6dc6ee50ae
gch4gas <gabrielgremista32@gmail.com>
2024-04-23 20:00:21 -0300

    Criando o corpo do site

---
+++ dataBase.js
@@ -0,0 +1,101 @@
+ export const places = [
+    {
+        view: "./imagens/Sao paulo.jpg",
+        cidade:"São Paulo"
+    },
+    {
+        view: "./imagens/tokyo.jpg",
+        cidade:"Tóquio"
+    },
+    {
+        view:"./imagens/Xunquim.jpg",
+        cidade:"Xunquim"
+    },
+    {
+        view: "./imagens/Déli.jpg",
+        cidade: "Déli"
+    },
+    {
+        view: "./imagens/Jacarta.jpg",
+        cidade:"Jacarta"
+    },
+
+]
+
+
+export const placesModal = [
+   {
+       city: "São Paulo",
+       history: "São Paulo, centro financeiro do Brasil, está entre as cidades mais populosas do mundo, com diversas instituições culturais e uma rica tradição arquitetônica. Há prédios simbólicos como a catedral neogótica, o Edifício Martinelli, um arranha-céu inaugurado em 1929, e o Edifício Copan, com suas linhas curvas projetadas pelo arquiteto modernista Oscar Niemeyer. A igreja em estilo colonial do Pátio do Colégio marca o local onde os padres jesuítas fundaram a cidade em 1554",
+       population: "12,33 milhões",
+       area: "1.521 km²",
+       density:"7.382,6 hab./km2"
+   },
+   {
+       city:"Tóquio",
+       history: "Tóquio, a movimentada capital do Japão, combina o estilo ultramoderno com o tradicional, desde arranha-céus iluminados por neon a templos históricos. O opulento santuário xintoísta Meiji é conhecido por seu altíssimo portão e pelas florestas circundantes. O Palácio Imperial fica localizado em meio a jardins públicos. Os muitos museus da cidade oferecem exposições que variam de arte clássica (no Museu Nacional de Tóquio) a um teatro kabuki reconstruído (no Museu Edo-Tokyo).",
+       population:"13,96 milhões",
+       area: "2.194 km²",
+       density:"6 354 hab./km²"
+   },
+   {
+        city:"Jacarta",
+        history:"Jacarta, a vasta capital da Indonésia, fica na costa noroeste da ilha de Java. Uma mistura histórica de culturas - javaneses, malaios, chineses, árabes, indianos e europeus - influenciou a arquitetura, o idioma e a culinária. No bairro antigo, Kota Tua, há edifícios coloniais holandeses, o Glodok (bairro chinês de Jacarta) e o antigo porto de Sunda Kelapa, onde atracam tradicionais escunas de madeira.",
+        population:"10,56 milhões",
+        area:"661,5 km²",
+        density:"120,7 hab/ km²",
+   },
+   {
+        city:"Déli",
+        history:"Délhi, a capital da Índia, é uma imensa área metropolitana no norte do país. Na Antiga Délhi, bairro que remonta ao século XVII, fica localizado o imponente Forte Vermelho, da era Mogol, que é um símbolo da Índia, e a grande mesquita Jama Masjid, cujo pátio acomoda 25.000 pessoas. Perto dali, fica o Chandni Chowk, um animado mercado repleto de bancas de comida, lojas de doces e barracas de especiarias.",
+        population:"11 milhões",
+        area:"1 483 km²",
+        density:"7 422,7 hab./km²"
+   },
+   {
+        city:"Xunquim",
+        history:"Chongqing é um grande município localizado na confluência entre os rios Yangtzé e Jialing, na região sudoeste da China. No centro da cidade, o complexo abobadado do Grande Salão do Povo se destaca sobre a Praça do Povo. Do outro lado da praça, o Museu das Três Gargantas apresenta artefatos da construção da Barragem das Três Gargantas, além de arte antiga.",
+        population:"31 milhões",
+        area:"82.339 km²",
+        density:"No have information"
+   }
+   
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
