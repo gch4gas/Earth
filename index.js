d09176cbb2f8c3f400ac478f5105f1e142666c35
gch4gas <gabrielgremista32@gmail.com>
2024-04-24 21:29:45 -0300

    construçao da logica

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-import { places } from "./dataBase";
+import  {placesModal}  from "./databaseModal.js";
+
+
+function showModal() {
+
+    const btnOpen = document.querySelector("#openModal");
+    const modalContainer = document.querySelector("#modalContainer");
+
+    
+    // modalContainer.innerHTML = ""
+    btnOpen.addEventListener('click', () => {
+
+        const createDescription = (place) => {
+            
+        
+            const pName = document.createElement("p");
+            const pHistory = document.createElement("p");
+            const pPopulation = document.createElement("p");
+            const pArea = document.createElement("p");
+            const pDensity = document.createElement("p");
+            const buttonModal = document.createElement("button");
+    
+            pName.innerText = place.city;
+            pHistory.innerText = place.history;
+            pPopulation.innerText = place.population;
+            pArea.innerText = place.area;
+            pDensity.innerText = place.density;
+            
+            modalContainer.append(pName,pHistory,pPopulation,pArea,pDensity);
+    
+            
+        }    
+
+    
+        modalContainer.showModal();
+        
+        closeModal();
+        
+   
+
+    const renderDescription = (arrayPlaces) => {
+
+
+        for(let i = 0; i < arrayPlaces.length; i++) {
+
+            const formDescription = arrayPlaces[i];
+            const description = createDescription(formDescription);
+
+            
+        }
+        
+    }
+        renderDescription(description);
+
+    })
+
+}
+
+function closeModal() {
+    const btnClose = document.querySelector("#closeModal");
+    const modalContainer = document.querySelector("#modalContainer");
+
+    btnClose.addEventListener('click', () => {
+        modalContainer.close()
+    })
+}
+showModal();
---
+++ indexDataBase.js
@@ -0,0 +1,3 @@
+import { places } from "./database";
+
+
